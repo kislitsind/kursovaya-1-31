0ba530c77181b8288ae021d558015c83f789faba
kislitsind <72073197+kislitsind@users.noreply.github.com>
2021-06-23 15:10:53 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+задача:Создать прямоугольную матрицу A, имеющую N строк и M столбцов со случайными элементами. Найти наибольший элемент столбца матрицы A, для которого сумма абсолютных значений элементов максимальна.
+
+import numpy as np
+print("Введите число строк:")
+n = int(input())
+print("Введите число столбцов:")
+m = int(input())
+arr = np.random.randint(10,20,(n,m))
+arrt = np.transpose(arr)
+arrabs = np.abs(arrt)
+sums = np.sum(arrabs, axis=1)
+max = np.argmax(sums)
+m = np.max(arrt[max])
+print(m)
